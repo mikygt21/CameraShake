23120fc1430023531c3db95ceee5a9487f306143
Miky21 <miquel.guspi@enti.cat>
2020-02-14 10:15:06 +0100

    done

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -9,16 +9,25 @@ public class CameraShake : MonoBehaviour
     private float m_damper = 0.0f;
     private float m_shake = 0.0f;
 
+    [Range(0f, 20f)]
+    [SerializeField] float m_maxvelocity;
+    [Space]
+
+    [Header("Sliders")]
+
     [SerializeField] Slider m_springSlider;
     [SerializeField] Slider m_damperSlider;
     [SerializeField] Slider m_shakeSlider;
 
-    public Vector2 PositionDiff = Vector2.zero;
+    public Vector2 PositionDiff { get { return m_initialPos - (Vector2)this.transform.position; } }
+
+    private Vector2 m_velocity;
+    private Vector2 m_initialPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_springSlider.onValueChanged.AddListener(delegate { m_spring = m_springSlider.value; });
+        m_springSlider.onValueChanged.AddListener(delegate { m_spring = Mathf.Min( Mathf.Max( m_springSlider.value, 0.1f)); });
         m_damperSlider.onValueChanged.AddListener(delegate { m_damper = m_damperSlider.value; });
         m_shakeSlider.onValueChanged.AddListener(delegate { m_shake = m_shakeSlider.value; });
 
@@ -30,12 +39,20 @@ public class CameraShake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(m_spring + " - " + m_damper + " - " + m_shake);
-
+        //Debug.Log(m_spring + " - " + m_damper + " - " + m_shake);
+        m_velocity += (m_initialPos - (Vector2)this.transform.position) * m_spring;
+        m_velocity -= m_velocity * m_damper;
+        transform.position = m_velocity;
     }
 
     public void AddShake(Vector2 input)
     {
+        m_velocity += input * m_shake;
+        m_velocity = new Vector2(Mathf.Clamp(-m_velocity.x, m_velocity.y, m_maxvelocity), Mathf.Clamp(-m_velocity.x, m_velocity.y, m_maxvelocity));
 
+        if(m_velocity.magnitude > m_maxvelocity) {
+            float index = m_velocity.magnitude / m_maxvelocity;
+            m_velocity *= (1f / index);
+        }
     }
 }
